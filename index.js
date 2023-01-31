c1554c45fe9c706067d55388ae65d5476010769d
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2023-01-31 08:00:31 +0100

    basic express setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("exporess");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+require("dotenv-flow").config();
+
+//routes
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, function(){
+    console.log("Aaargh!: " + port)
+});
+
+module.exports = app;
