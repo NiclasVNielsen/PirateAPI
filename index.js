52bcc901045013356af5a35ac3ddd861e866d94c
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2023-02-01 20:10:33 +0100

    simple example route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 require("dotenv-flow").config();
 
 //routes
+app.get("/api/aaargh", (req, res) => {
+    res.status(200).send({msg: "meep"})
+})
 
 const port = process.env.PORT || 8000;
 
